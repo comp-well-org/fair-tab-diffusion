54e997e4caffae6eee98bf81aece26b68f78d80d
Zeyu Yang <zeyuyang8@gmail.com>
2024-07-18 17:41:02 -0500

    .

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import argparse
+import warnings
 import tomli
+import os
+import torch
+import shutil
 import numpy as np
 from src.diffusion.utils import XYCTabDataModule
 from src.diffusion.estimator import PosteriorEstimator, DenoiseFn
@@ -8,9 +12,18 @@ from src.diffusion.unet import Unet
 from src.diffusion.ddpm import GaussianMultinomialDiffusion
 from src.diffusion.trainer import XYCTabTrainer
 
+warnings.filterwarnings('ignore')
+
 def load_config(path) -> dict:
     with open(path, 'rb') as f:
         return tomli.load(f)
+    
+def copy_file(exp_dir, config_path) -> None:
+    if not os.path.exists(exp_dir):
+        os.makedirs(exp_dir)
+        shutil.copy(config_path, exp_dir)
+    else:
+        shutil.copy(config_path, exp_dir)
 
 def main():
     parser = argparse.ArgumentParser()
@@ -27,16 +40,39 @@ def main():
     guid_config = config['guid']
     data_config = config['data']
     model_config = config['model']
-    sample_config = config['sample']
+    
+    # message
+    print(f'experiment: {exp_config["name"]}')
+    print('-' * 80)
+    
+    # whether to use fair diffusion model
+    is_fair = exp_config['fair']
+    
+    # random seed
+    seed = exp_config['seed']
+    n_seeds = exp_config['n_seeds']
+    assert n_seeds > 0, '`n_seeds` must be greater than 0'
+    torch.manual_seed(seed)
+    
+    # experimental directory
+    exp_dir = os.path.join(
+        exp_config['home'], 
+        exp_config['name'], 
+        data_config['name'],
+        exp_config['method'],
+    )
+    
+    copy_file(
+        os.path.join(exp_dir), 
+        args.config,
+    )
     
     data_module = XYCTabDataModule(
-        root='/rdf/db/public-tabular-datasets/adult/',
-        batch_size=32,
+        root=os.path.join(data_config['path'], data_config['name']),
+        batch_size=data_config['batch_size'],
     )
 
     data_desc = data_module.get_data_description()
-    norm_fn = data_module.get_norm_fn()
-    empirical_dist = data_module.get_empirical_dist()
 
     d_oh_x = data_desc['d_oh_x']
     d_num_x = data_desc['d_num_x']
@@ -44,8 +80,10 @@ def main():
     n_unq_c_lst = data_desc['n_unq_c_lst']
     n_unq_cat_od_x_lst = data_desc['n_unq_cat_od_x_lst']
     
-    cond_emb_factor = 2
+    is_fair = exp_config['fair']
+    cond_emb_factor = 3 if is_fair else 2
     
+    # denoising function
     denoise_fn = DenoiseFn(
         denoise_fn_cfg=DenoiseFnCfg(
             d_x_emb=model_config['d_x_emb'],
@@ -82,6 +120,7 @@ def main():
         ),
     )
     
+    # diffusion
     diffusion = GaussianMultinomialDiffusion(
         num_classes=np.array(n_unq_cat_od_x_lst),
         num_numerical_features=d_num_x,
@@ -90,7 +129,7 @@ def main():
         scheduler=model_config['scheduler'],
         max_beta=model_config['max_beta'],
         num_timesteps=model_config['n_timesteps'],
-        is_fair=False,
+        is_fair=is_fair,
         gaussian_parametrization=model_config['parametrization'],
     )
     
@@ -101,10 +140,10 @@ def main():
         lr=train_config['lr'],
         weight_decay=train_config['weight_decay'],
         max_non_improve=train_config['max_non_improve'],
-        is_fair=False,
+        is_fair=is_fair,
         device=exp_config['device'],
     )
-    trainer.fit(diffusion, data_module, './fair-tab-diffusion-exps')
+    trainer.fit(diffusion, data_module, exp_dir)
 
 if __name__ == '__main__':
     main()
