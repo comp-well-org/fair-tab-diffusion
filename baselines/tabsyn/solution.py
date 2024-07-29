6a9b8c914f23ab5d43d69504a7bc431b45ad9bf4
Zeyu Yang <zeyuyang8@gmail.com>
2024-07-29 10:58:22 -0500

    .

--- baselines/tabsyn/solution.py
+++ baselines/tabsyn/solution.py
@@ -677,87 +677,24 @@ class EDMLoss:
 
 ################################################################################
 # latent utils
-def get_input_generate(args):
-    dataname = args.dataname
-
-    curr_dir = os.path.dirname(os.path.abspath(__file__))
-    dataset_dir = f'data/{dataname}'
-    ckpt_dir = f'{curr_dir}/ckpt/{dataname}'
-
-    with open(f'{dataset_dir}/info.json', 'r') as f:
-        info = json.load(f)
-
-    task_type = info['task_type']
-
-
-    ckpt_dir = f'{curr_dir}/ckpt/{dataname}'
-
-    _, _, categories, d_numerical, num_inverse, cat_inverse = preprocess(dataset_dir, task_type = task_type, inverse = True)
-
-    embedding_save_path = f'{curr_dir}/vae/ckpt/{dataname}/train_z.npy'
-    train_z = torch.tensor(np.load(embedding_save_path)).float()
-
-    train_z = train_z[:, 1:, :]
-
-    B, num_tokens, token_dim = train_z.size()
-    in_dim = num_tokens * token_dim
-    
-    train_z = train_z.view(B, in_dim)
-    pre_decoder = Decoder_model(2, d_numerical, categories, 4, n_head = 1, factor = 32)
-
-    decoder_save_path = f'{curr_dir}/vae/ckpt/{dataname}/decoder.pt'
-    pre_decoder.load_state_dict(torch.load(decoder_save_path))
-
-    info['pre_decoder'] = pre_decoder
-    info['token_dim'] = token_dim
-
-    return train_z, curr_dir, dataset_dir, ckpt_dir, info, num_inverse, cat_inverse
-
 @torch.no_grad()
-def split_num_cat_target(syn_data, info, num_inverse, cat_inverse, device):
-    task_type = info['task_type']
-
-    num_col_idx = info['num_col_idx']
-    cat_col_idx = info['cat_col_idx']
-    target_col_idx = info['target_col_idx']
-
-    n_num_feat = len(num_col_idx)
-    n_cat_feat = len(cat_col_idx)
-
-    if task_type == 'regression':
-        n_num_feat += len(target_col_idx)
-    else:
-        n_cat_feat += len(target_col_idx)
-
-    pre_decoder = info['pre_decoder']
-    token_dim = info['token_dim']
-
+def split_num_cat_target(syn_data, categories, d_numerical, pre_decoder, token_dim):
     syn_data = syn_data.reshape(syn_data.shape[0], -1, token_dim)
-    
-    norm_input = pre_decoder(torch.tensor(syn_data))
+    syn_data = torch.tensor(syn_data)
+    pre_decoder = pre_decoder.to(syn_data.device)
+    norm_input = pre_decoder(syn_data)
     x_hat_num, x_hat_cat = norm_input
 
     syn_cat = []
     for pred in x_hat_cat:
-        syn_cat.append(pred.argmax(dim = -1))
+        syn_cat.append(pred.argmax(dim=-1))
 
     syn_num = x_hat_num.cpu().numpy()
     syn_cat = torch.stack(syn_cat).t().cpu().numpy()
 
-    syn_num = num_inverse(syn_num)
-    syn_cat = cat_inverse(syn_cat)
+    return syn_num, syn_cat
 
-    if info['task_type'] == 'regression':
-        syn_target = syn_num[:, :len(target_col_idx)]
-        syn_num = syn_num[:, len(target_col_idx):]
-    
-    else:
-        print(syn_cat.shape)
-        syn_target = syn_cat[:, :len(target_col_idx)]
-        syn_cat = syn_cat[:, len(target_col_idx):]
-
-    return syn_num, syn_cat, syn_target
-
+# TODO: implement this function
 def recover_data():
     pass
 
@@ -930,32 +867,9 @@ def train_diffusion_model(
 
 ################################################################################
 # sampling
-def sampling_synthetic_data(model, dataname, device, steps, save_path, ckpt_dir):
-    train_z, categories, d_numerical = get_input_generate(ckpt_dir)
-    in_dim = train_z.shape[1] 
-    mean = train_z.mean(0)
-    denoise_fn = MLPDiffusion(in_dim, 1024).to(device)
-    
-    model = Model(denoise_fn=denoise_fn, hid_dim=train_z.shape[1]).to(device)
-    model.load_state_dict(torch.load(f'{ckpt_dir}/diffusion.pt'))
-
-    start_time = time.time()
-    num_samples = train_z.shape[0]
-    sample_dim = in_dim
-
-    x_next = sample(model.denoise_fn_D, num_samples, sample_dim)
-    x_next = x_next * 2 + mean.to(device)
-
-    syn_data = x_next.float().cpu().numpy()
-    syn_num, syn_cat, syn_target = split_num_cat_target(syn_data, categories, d_numerical, device) 
-
-    syn_df = recover_data(syn_num, syn_cat, syn_target, dataname)
-
-    syn_df.to_csv(save_path, index=False)
-    end_time = time.time()
-    
-    print(f'sampling time: {end_time - start_time:.2f}s')
-    print(f'saving sampled data to {save_path}')
+# TODO: implement this function
+def sampling_synthetic_data():
+    pass
     
 def main():
     # global variables
@@ -1044,7 +958,7 @@ def main():
         model, optimizer, scheduler, n_epochs, train_loader, ckpt_dir, device,
     )
 
-    # # sampling
+    # sampling
     # sampling_synthetic_data(model)
     
     in_dim = train_z.shape[1] 
@@ -1062,15 +976,25 @@ def main():
     x_next = x_next * 2 + mean.to(device)
 
     syn_data = x_next.float().cpu().numpy()
-    # syn_num, syn_cat, syn_target = split_num_cat_target(syn_data, categories, d_numerical, device) 
-
-    # syn_df = recover_data(syn_num, syn_cat, syn_target, dataname)
-
-    # syn_df.to_csv(save_path, index=False)
-    # end_time = time.time()
     
-    # print(f'sampling time: {end_time - start_time:.2f}s')
-    # print(f'saving sampled data to {save_path}')
+    embedding_save_path = f'{ckpt_dir}/train_z.npy'
+    train_z = torch.tensor(np.load(embedding_save_path)).float()
+    train_z = train_z[:, 1:, :]
+    B, num_tokens, token_dim = train_z.size()
+    
+    syn_num, syn_cat = split_num_cat_target(syn_data, categories, d_numerical, pre_decoder, token_dim)
+    # print(syn_num.shape, syn_cat.shape)
+    
+    dn_syn = np.concatenate([syn_num, syn_cat], axis=1)
+    dn_syn = pd.DataFrame(dn_syn)
+    print(dn_syn.head(3))
+    
+    # NOTE: this generates synthetic data consisting of normalized numerical and categorical features, and target is in the last column of categorical features
+    # syn_df = recover_data(syn_num, syn_cat, dataname)
+    
+    end_time = time.time()
+    
+    print(f'sampling time: {end_time - start_time:.2f}s')
 
 if __name__ == '__main__':
     main()
