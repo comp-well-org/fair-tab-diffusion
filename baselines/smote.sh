59e249e44ce6e8bdfc580585018f1edc2a2c14ee
Zeyu Yang <zeyuyang8@gmail.com>
2024-08-11 23:14:06 -0500

    .

---
+++ baselines/smote.sh
@@ -0,0 +1,5 @@
+python smote_opt.py --dataset german --n_trials 10
+python smote_opt.py --dataset adult --n_trials 10
+python smote_opt.py --dataset compass --n_trials 10
+python smote_opt.py --dataset bank --n_trials 10
+python smote_opt.py --dataset law --n_trials 10
--- run.sh
+++
@@ -1,5 +0,0 @@
-python fairtabddpm_opt.py --dataset german
-python fairtabddpm_opt.py --dataset adult
-python fairtabddpm_opt.py --dataset compass
-python fairtabddpm_opt.py --dataset bank
-python fairtabddpm_opt.py --dataset law
