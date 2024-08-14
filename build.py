c3b8a8f42a32120f904663a86ceadf97bca26598
Zeyu Yang <zeyuyang8@gmail.com>
2024-08-14 12:48:03 -0500

    .

--- build.py
+++ build.py
@@ -6,8 +6,7 @@ def main():
     save_openml_dataset('adult', dir_path=os.path.join(DB_PATH, 'adult'))
     save_openml_dataset('bank', dir_path=os.path.join(DB_PATH, 'bank'))
     save_openml_dataset('compass', dir_path=os.path.join(DB_PATH, 'compass'))
-    # save_openml_dataset('law', dir_path=os.path.join(DB_PATH, 'law'))
-    # save_openml_dataset('german', dir_path=os.path.join(DB_PATH, 'german'))
+    save_openml_dataset('german', dir_path=os.path.join(DB_PATH, 'german'))
 
 if __name__ == '__main__':
     main()
