e92c0223862b4651f62857cb0ebab5296075f04f
Zeyu Yang <zeyuyang8@gmail.com>
2024-08-16 13:31:21 -0500

    .

--- assess/figure.sh
+++ assess/figure.sh
@@ -1,1 +1,4 @@
 python datavis.py --dataset compass --plot_dist
+python datavis.py --dataset german --plot_dist
+python datavis.py --dataset adult --plot_dist
+python datavis.py --dataset bank --plot_dist
